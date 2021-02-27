cd147cd0dfc678226d837534f7465bbeb83fc8f4
Scott47 <scottsilver47@gmail.com>
2021-02-27 17:13:33 -0600

    added nav bar

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,10 +1,15 @@
 import React from "react";
 import '../App.css';
 import { ApplicationViews } from './ApplicationViews';
+import { NavBar } from './nav/NavBar'
 
 const KandyKorner = () => {
     return (
-        <ApplicationViews />
+        <>
+            <NavBar />
+            <ApplicationViews />
+        </>
+
     )
 }
 export default KandyKorner
